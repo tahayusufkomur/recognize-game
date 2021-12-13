82c40f47ad043d8aa12b7d60c8c7dbdfd6054208
Taha Yusuf <47995691+tahayusufkomur@users.noreply.github.com>
2021-12-13 03:58:24 +0300

    added some comments, closed file pointer

--- RecognizingGame.py
+++ RecognizingGame.py
@@ -3,6 +3,7 @@ import random
 import sys, getpass
 import os
 
+## Check if files exists
 if (os.path.exists('words.txt') == False):
     with open('words.txt', 'w') as f:
         f.write("interpretation = tercüme")
@@ -13,21 +14,27 @@ if (os.path.exists('sentences.txt') == False):
         f.write("existentialism, any of various philosophies, most influential in continental Europe from about 1930 to the mid-20th century, that have in common an interpretation of human existence in the world that stresses its concreteness and its problematic character. = varoluşçuluk, yaklaşık 1930'dan 20. yüzyılın ortalarına kadar kıta Avrupası'nda en etkili olan, dünyadaki insan varoluşunun somutluğunu ve sorunlu karakterini vurgulayan ortak bir yorumuna sahip çeşitli felsefelerden herhangi biri. ")
     f.close()
 
+## construct words dictionary
 words = {}
+
 f = open("words.txt")
 for line in f:
     key, value = line.split('=')
     words[key] = value
 f.close()
 
+## construct sentences dictionary
 sentences = {}
+
 f = open("sentences.txt")
 for line in f:
     key, value = line.split('=')
     sentences[key] = value
 f.close()
 
+## adds element to given dictionary
 def add_element(dictionary):
+
         string = input()
         if(string == "exit"):
             return False
@@ -35,7 +42,9 @@ def add_element(dictionary):
         dictionary[key] = value
         return True
 
+## starts the game
 def play_game(dictionary):
+        
         true_answers = 0
         total = 0
         string = input("Press enter to start the game! \(exit+enter to exit\) ")
@@ -59,11 +68,13 @@ def play_game(dictionary):
                 print("False! Score:" +str(true_answers)+'/'+str(total))
             
         return score
-    
+
+## at the end, writes updated dictionary to the file
 def write_dict_to_file(dictionary, file_name):
     with open(file_name, 'w') as f:
            for key, value in dictionary.items():
                  f.write(str(key)+ '='+ str(value))
+            f.close()
 
 print ("""Menu
 1. Add a word with =
